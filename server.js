7bab9ccfe161ba5e3ab7bc07acd502c0aa1941a2
Viplav Nayyar <52647445+Viplav1996@users.noreply.github.com>
2021-01-26 21:06:20 -0500

    Server working, baby steps from client to server to db via api and all the way back

--- server.js
+++ server.js
@@ -4,8 +4,28 @@ var bodyParser = require('body-Parser');
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/FullStackWeb');
 
+var Product = require('./model/product');
+var Wishlist = require('./model/wishList');
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+
+app.post('/product', function(req, res) {
+    var product = new Product();
+    product.title = req.body.title;
+    product.price = req.body.price;
+    product.save(function(err, saveProduct){
+        if (err) {
+
+            res.status(500).send({error: "Could not save product"});
+
+        } else {
+            res.send(saveProduct);
+        }
+    })
+});
+
 
 app.listen(3000, function (){
     console.log("Swag shop API running on port 3000...!!!")
